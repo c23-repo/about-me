243b93e43ca3fb331af1c2414366b387bd440884
Cristian Restrepo <crestrepo21@gmail.com>
2019-03-27 00:04:28 -0700

    js-portion-done

--- js/app.js
+++ js/app.js
@@ -1,31 +1,61 @@
 'use strict';
 
+var guessGame = prompt('Hello, do you want to play a game?');
+if(guessGame === 'Yes' || guessGame === 'yes' || guessGame === 'YES'){
+  alert('Cool, let\'s play.');
+  console.log('user answered correctly with yes');
+} else if ( guessGame === 'no' || guessGame === 'No' || guessGame === 'NO'){
+  alert('OK, well, we\'re going to play anyway.');
+  console.log('user chose incorrectly with no');
+} else { 
+  alert('please say YES or NO');
+  console.log('user answered incorrectly');
+  }
+
 var mySport = prompt('Do you think that I play baseball?');
   if(mySport === 'No' || mySport === 'no' || mySport === 'NO'){
     alert('You are correct');
-    console.log('user answered correctly with no')
+    console.log('user answered correctly with no');
   } else if ( mySport === 'yes' || mySport === 'Yes' || mySport === 'YES'){
     alert('Sorry, but that\'s incorrect');
     console.log('user chose incorrectly with yes');
-  } else { 
+  } else{ 
     alert('please say YES or NO');
-    console.log('user answered incorrectly')
+    console.log('user answered incorrectly');
     }
 
 var smart = prompt('Do you think that I\'m smart?').toUpperCase();
-  if(smart === 'YES' || 'Y'){
+  if(smart === 'YES' || smart === 'Y'){
     alert('OH, why thank you. You are very perceptive');
     console.log('user answered correctly');
   } else{
-    alert('Really? it\'s like that.')
-    console.log('user answered incorrectly')
+    alert("Really? it\'s like that.");
+    console.log('user answered incorrectly');
   }
 
-var marine = prompt('Am I a Marine?').toLowerCase();
-if(marine ==='yes' || marine === 'y'){
-  alert('You are correct');
-  console.log('user guessed right');
-} else{
-  alert('sorry, you are the weakest link')
-  console.log('user guessed wrong');
-}
+var marine = prompt(' Do you think that I am a Marine?').toLowerCase();
+  if(marine === 'yes' || marine === 'y'){
+    alert('Is that why you guessed I wasn\'t smart?');
+    console.log('user guessed right');
+  } else{
+    alert('sorry, you are the weakest link');
+    console.log('user guessed wrong');
+  }
+
+var dogs = prompt('Do you think that I own dogs?').toLowerCase();
+  if(dogs === 'yes' || dogs ==='y'){
+    alert('I do have dogs!');
+    console.log('user guessed correctly that I have dogs')
+  } else{
+    alert('sorry, I have two dogs');
+    console.log('user guessed incorrectly that I had no dogs')
+  }
+
+var singer = prompt('Do you think I can sing?').toLowerCase();
+  if(singer === 'no' || singer === 'n'){
+    alert('Yeah, I can\'t sing at all.');
+    console.log('user guessed correctly that I can not sing');
+  } else {
+    alert('Sorry to disappoint you.');
+    console.log('user guessed incorrectly that I could sing');
+  }
