5c8ec7718fdca7ed54b8420bdbf77b5ad073a15f
Paula Thomas <paula9t9@gmail.com>
2019-03-28 18:28:28 -0700

    Y/N questions work with new function

--- js/app.js
+++ js/app.js
@@ -1,60 +1,46 @@
 'use strict';
 
+var yesResponseArray = ['yes', 'y','true','t'];
+var noResponseArray = ['no', 'n', 'false','f'];
+var answersArray = [false, true, true, false, true, '4',  countriesArray];
+var countriesArray = ['colombia', 'japan', 'afghanistan'];
+var questionsArray = [', do you think that I play baseball?', ', do you think that I own a car?', ', do you think that I am a Marine?', ', do you think that I own dogs?', ', do you think I can cook?','Can you guess my Jersey number in rugby?','Can you guess what countries I have been to?']
+
+function checkAnswer (answer) {
+  if(yesResponseArray.includes(answer.toLowerCase())){
+    console.log('Input equates to true');
+    return true;
+  } else if(noResponseArray.includes(answer.toLowerCase())){
+    console.log('Input equates to false');
+    return false;
+  } else {
+    console.log('Unrecognized');
+    return answer;
+  }
+
+}  
+
+
+
 var userName = prompt('Hello, what is your name?');
 console.log( 'user input name')
 
-var mySport = prompt(userName+', do you think that I play baseball?').toLowerCase();
-  if(mySport === 'n' || mySport === 'no'){
-    alert('You are correct');
-    console.log('user answered correctly with no');
-  } else if ( mySport === 'yes' || mySport === 'Yes' || mySport === 'YES'){
-    alert('You are incorrect');
-    console.log('user chose incorrectly with yes');
-  } else{ 
-    alert('please say YES or NO');
-    console.log('user answered incorrectly');
-    }
-
-var car = prompt(userName+', do you think that I own a car?').toUpperCase();
-  if(car === 'NO' || car === 'N'){
-    alert('You are correct');
-    console.log('user answered correctly');
-  } else{
-    alert("You are incorrect.");
-    console.log('user answered incorrectly');
-  }
-
-var marine = prompt(userName+', do you think that I am a Marine?').toLowerCase();
-  if(marine === 'yes' || marine === 'y'){
-    alert('You are correct');
-    console.log('user guessed right');
-  } else{
-    alert('You are incorrect.');
-    console.log('user guessed wrong');
-  }
-
-var dogs = prompt(userName+', do you think that I own dogs?').toLowerCase();
-  if(dogs === 'yes' || dogs ==='y'){
-    alert('You are correct');
-    console.log('user guessed correctly that I have dogs')
-  } else if(dogs === 'no' || dogs === 'n'){
-    alert('You are incorrect.');
-    console.log('user guessed incorrectly that I had no dogs')
-  } else 
-
-var cook = prompt(userName+', do you think I can cook?').toLowerCase();
-  if(cook === 'yes' || cook === 'y'){
-    alert('You are correct');
-    console.log('user guessed correctly that I can not sing');
+for (var i = 0; i < questionsArray.length; i++){
+  var userAnswer = prompt(userName + questionsArray[i]);
+  if(answersArray[i] === checkAnswer(userAnswer)){
+    console.log('User answered correctly');
+    alert('You are correct.');
   } else {
     alert('You are incorrect.');
-    console.log('user guessed incorrectly that I could sing');
   }
+}
+
+
 var jerseyNum = '';
 var limit=3;
 var guesses=0
   while(jerseyNum !== '4' && guesses<limit){
-    jerseyNum=prompt('Can you guess my Jersey number in rugby?');
+    jerseyNum=prompt(questionsArray[5]);
     guesses++;
   } console.log('user guessed jersey number');
 
@@ -64,11 +50,12 @@ var guesses=0
   var score=0;
   var userInput='';
       while(userInput !== countries && guesses<limitOconnus){
-        userInput= prompt('Can you guess what countries I have been to?').toLowerCase()
+        userInput= prompt(questionsArray[6]).toLowerCase()
         guesses++;
         score++;
       }alert('All the possible answers were Colombia, Japan, and Afghanistan. You got ' + score + ' out of 3 countries.');
      console.log( userInput);
 
+   
 
 //recieved guidance and collaborated with Tim Busch, Chaitanya Narukulla, David Marchante
